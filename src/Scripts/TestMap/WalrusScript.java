a0d6474b955ceeac52dc2b8b78d468f194d2480e
Raffaele Valentino <rdvalentino@dhcp-10-111-57-45.quinnipiac.edu>
2026-09-22 17:34:54 -0400

    removed a new line that wasn't necessary

--- src/Scripts/TestMap/WalrusScript.java
+++ src/Scripts/TestMap/WalrusScript.java
@@ -1,9 +1,7 @@
 package Scripts.TestMap;
-
-import java.util.ArrayList;
-
 import Level.Script;
 import ScriptActions.*;
+import java.util.ArrayList;
 
 // script for talking to walrus npc
 // checkout the documentation website for a detailed guide on how this script works
