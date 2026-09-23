1cd63150456906e281c0ed4fb2d0410da78330e9
CaseyClausen33 <cmclausen06@gmail.com>
2026-09-23 18:17:45 -0400

    just fixed a small bug

--- src/Maps/NewMap.java
+++ src/Maps/NewMap.java
@@ -2,7 +2,7 @@ package Maps;
 
 import EnhancedMapTiles.PushableRock;
 import Level.*;
-import NPCs.Bug;
+import NPCs.Ghost;
 import NPCs.Dinosaur;
 import NPCs.Walrus;
 import Scripts.ChangeMapScript;
